adb80fbd094d3d29e5f13a67546b4bf8c43f675d
Sungho Cho <sunghoch@andrew.cmu.edu>
2022-05-04 23:24:07 -0600

    Add conversion.py that converts between grids and actions

--- gym_janggi/conversion.py
+++ gym_janggi/conversion.py
@@ -1,1 +1,24 @@
-import .janggi_game
+from .janggi_game import JanggiGame
+from classes.grid import Grid
+import classes.constant as constant
+
+
+def gridsToAction(origin: Grid, dest: Grid):
+    (originRow, originCol) = origin
+    (destRow, destCol) = dest
+
+    originRow = 0 if originRow == constant.MAX_ROW else originRow
+    destRow = 0 if destRow == constant.MAX_ROW else destRow
+
+    return originRow * 1000 + originCol * 100 + destRow * 10 + destCol
+
+
+def actionToGrids(action: int):
+    originRow = action // 1000
+    originCol = action // 100 % 10
+    destRow = action // 10 % 10
+    destCol = action % 10
+
+    origin = Grid(originRow, originCol)
+    dest = Grid(destRow, destCol)
+    return origin, dest
