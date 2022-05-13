e9db75ecfe045d8d8a8eb14ff99f00b5d2216acf
Sungho Cho <didog9595@gmail.com>
2022-05-13 18:26:34 +0900

    Add README.md and LICENSE for packaging

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="gym_janggi",
-    version="0.0.1",
-    install_requires=["gym==0.23.1", "janggi==0.0.1"],
+    version="0.9.0",
+    author="Sungho Cho",
+    author_email="didog9595@gmail.com",
+    description="OpenAI Gym environments for Korean chess Janggi",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sungho-cho/gym-janggi",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["gym_janggi"],
+    python_requires=">=3.6",
+    install_requires=["gym==0.23.1", "janggi==0.9.0"],
 )
